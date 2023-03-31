8e6f72581dc792d0a3b6e86031f164e7ed42eb8c
ankitaniket <ankitaniket3@gmail.com>
2023-03-31 20:17:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
+// import { useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Department of Pothole Management</h1>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<EmpListing />} />
+          <Route path='/employee/create' element={<EmpCreate />}></Route>
+
+          <Route path='/admin/employee/detail/:empid' element={<EmpDetail />}></Route>
+          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+
+}
+
+export default App;
